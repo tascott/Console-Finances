7709c8aa591780557fb6c67f97a4f50875fe58cc
TomScott <contact@tascott.co.uk>
2022-12-18 19:49:01 +0000

    Put the all the month on month changes in to a new array, get the average

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 console.log(`Months: ${finances.length}`);
 
 let total = 0;
+let changes =[];
 
 // Used x instead of i as i looked too similar to 1
 for (let x = 0, len = finances.length; x < len; x++) {
     // Add the profit or loss of each month to a variable
     total += finances[x][1];
 
+    if (finances[x+1]) { // check there is still two items to compare
+        // Subtract the difference between item i and the one before it, and push to [changes] array
+        changes.push(finances[x+1][1] - finances[x][1])
+    }
+
 }
 
 console.log(`Net p/l: ${total}`);
 
+// [Changes is now an array of the difference between each month]
+// Add every item together and divide by the length for an average
+const average = changes.reduce((a, b) => a + b, 0) / changes.length;
 
+
+console.log(`Average change: ${average}`)
