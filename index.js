cdbd965ce6e019e6e8ef48a550bd8192fd475e63
TomScott <contact@tascott.co.uk>
2022-12-18 19:39:34 +0000

    Loop over the finances array, add the p/l together

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 console.log(`Months: ${finances.length}`);
 
+let total = 0;
+
+// Used x instead of i as i looked too similar to 1
+for (let x = 0, len = finances.length; x < len; x++) {
+    // Add the profit or loss of each month to a variable
+    total += finances[x][1];
+
+}
+
+console.log(`Net p/l: ${total}`);
+
 
