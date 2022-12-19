ecde1b8f25d7e42b31314a75a32fa4e72f9528f6
TomScott <contact@tascott.co.uk>
2022-12-19 21:12:50 +0000

    Change some variable names for readability

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 let total = 0;
-let changes =[];
+let monthOnMonthChange =[];
 // Make a new array to play with. Spread finances array into it
 let updatedFinances = [...finances];
 
 function calculate(){
-    // Used x instead of i as i looked too similar to 1
     for (let x = 0, len = updatedFinances.length; x < len; x++) {
         // Add the profit or loss of each month to a variable
         total += updatedFinances[x][1];
 
         if (updatedFinances[x+1]) { // check there is still two items to compare
             // Subtract the difference between item i and the one before it, and push to [changes] array
-            changes.push(updatedFinances[x+1][1] - updatedFinances[x][1]);
+            monthOnMonthChange.push(updatedFinances[x+1][1] - updatedFinances[x][1]);
             // Add an item to each month to show the difference betwen the changes
             updatedFinances[x+1].push(updatedFinances[x+1][1] - updatedFinances[x][1]);
         }
@@ -52,7 +51,9 @@ calculate(); // Run on first load with current data
 
 
 var form = document.getElementById("new-data");
-function handleForm(event) {
+
+
+function addNewMonth(event) {
     event.preventDefault();
     let date = document.getElementById('date')
     let amount = document.getElementById('profit')
@@ -62,4 +63,5 @@ function handleForm(event) {
     amount.value = '';
 
 }
-form.addEventListener('submit', handleForm);
+
+form.addEventListener('submit', addNewMonth);
